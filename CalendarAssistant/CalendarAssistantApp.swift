69f305b8ce80be6266e8567b8eed27f4b937db55
sibisai <u.sibisai@gmail.com>
2025-04-04 13:00:14 -0700

    Initial Commit

---
+++ CalendarAssistant/CalendarAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAssistantApp.swift
+//  CalendarAssistant
+//
+//  Created by Sibi on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAssistantTests/CalendarAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAssistantTests.swift
+//  CalendarAssistantTests
+//
+//  Created by Sibi on 4/4/25.
+//
+
+import Testing
+@testable import CalendarAssistant
+
+struct CalendarAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
